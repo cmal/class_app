be1dabe431ce0f580dc8c33011e227edbe8ec45f
cmal <zyzy5730@163.com>
2016-03-27 23:28:31 +0800

    home page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,11 @@
+# coding: utf-8
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "班级管理系统"
   end
 
 end
