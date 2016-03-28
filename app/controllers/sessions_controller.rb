17432ba9bc7eb4aa04c7e534b2c700a131b2dda5
cmal <zyzy5730@163.com>
2016-03-28 11:20:47 +0800

    user login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+# coding: utf-8
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user, flash: { success: "已登入" }
+    else
+      flash.now[:danger] = "电子邮箱或密码错误!"
+      render "new"
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url, flash: { info: "已登出" }
+    #flash.now[:info] = "已登出"
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "已成功注册！"
       redirect_to @user
     else
